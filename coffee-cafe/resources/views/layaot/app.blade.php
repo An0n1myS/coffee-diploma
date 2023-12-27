415682995d71e7e25dfabb56327f56f9e98f4270
An0n1myS <hds2014228@gmail.com>
2023-12-27 12:29:28 +0200

    Initial commit

---
+++ coffee-cafe/resources/views/layaot/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Coffee cafe</title>
+</head>
+<body class>
+<div class="">
+    <div class="">
+        @include('partials.header')
+
+        @yield('content')
+
+        @include('partials.footer')
+    </div>
+</div>
+
+
+@vite('resources/js/app.js')
+</body>
+</html>
